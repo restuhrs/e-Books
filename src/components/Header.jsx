c0128ed966d0e59d71cd56e2cc8d91fc77d25059
Restu <restukharisma858@gmail.com>
2025-08-13 13:25:08 +0700

    feat & refactor: refresh color

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,13 @@
 function Header() {
-    return (
-        <header className="bg-gradient-to-r from-slate-900 to-slate-700 text-white-shadow">
-            <div className="container mx-auto px-1 py-6">
-                <h1 className="text-3xl font-bold bg-gradient-to-r bg-clip-text text-white">
-                    Perpustakaan Sederhana</h1>
-            </div>
-        </header>
-    );
+  return (
+    <header className="bg-gradient-to-r from-blue-500 to-teal-400 text-white-shadow">
+      <div className="container mx-auto px-1 py-6">
+        <h1 className="text-3xl font-bold bg-gradient-to-r bg-clip-text text-white">
+          e-Books
+        </h1>
+      </div>
+    </header>
+  );
 }
 
-export default Header
+export default Header;
