eabc80322a036ceeaec5eb401dec667e7478d39a
Restu <restukharisma858@gmail.com>
2025-09-17 16:22:42 +0700

    add spinner

--- src/components/auth/ProtectedRoute.jsx
+++ src/components/auth/ProtectedRoute.jsx
@@ -4,7 +4,32 @@ import { useAuth } from "../../hooks/useAuth";
 export default function ProtectedRoute({ children }) {
   const { user, loading } = useAuth();
 
-  if (loading) return <p>Loading...</p>;
+  if (loading)
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <svg
+          className="animate-spin h-8 w-8 text-amber-600"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+          ></path>
+        </svg>
+      </div>
+    );
+
   if (!user) return <Navigate to="/login" replace />;
 
   return children;
--- src/components/auth/ProtectedRouteWithRole.jsx
+++ src/components/auth/ProtectedRouteWithRole.jsx
@@ -5,7 +5,30 @@ export default function ProtectedRouteWithRole({ children, allowedRoles }) {
   const { user, role, loading } = useAuth();
 
   if (loading) {
-    return <p>Loading...</p>; // atau spinner loading
+    return (
+      <div className="flex justify-center items-center h-screen">
+        <svg
+          className="animate-spin h-8 w-8 text-amber-600"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+        >
+          <circle
+            className="opacity-25"
+            cx="12"
+            cy="12"
+            r="10"
+            stroke="currentColor"
+            strokeWidth="4"
+          ></circle>
+          <path
+            className="opacity-75"
+            fill="currentColor"
+            d="M4 12a8 8 0 018-8v4a4 4 0 00-4 4H4z"
+          ></path>
+        </svg>
+      </div>
+    );
   }
 
   if (!user) {
@@ -13,7 +36,7 @@ export default function ProtectedRouteWithRole({ children, allowedRoles }) {
   }
 
   if (!allowedRoles.includes(role)) {
-    return <Navigate to="/" replace />; // bisa diarahkan ke halaman lain (misalnya 403 page)
+    return <Navigate to="/" replace />;
   }
 
   return children;
